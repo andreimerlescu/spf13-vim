e67b819cd65c3c98ea3505b1967f87983f9e6f96
Andrei Merlescu <andrei+github@merlescu.net>
2024-12-30 18:08:57 -0500

    Update bootstrap.sh
    Set the URI to https:// from git:// to allow un-configured SSH users to access the installation of this unbroken spf-13 vim installer.

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,7 +17,7 @@
 ############################  SETUP PARAMETERS
 app_name='spf17-vim'
 [ -z "$APP_PATH" ] && APP_PATH="$HOME/.spf17-vim-3"
-[ -z "$REPO_URI" ] && REPO_URI='git@github.com:andreimerlescu/spf13-vim.git'
+[ -z "$REPO_URI" ] && REPO_URI='https://github.com/andreimerlescu/spf13-vim.git'
 [ -z "$REPO_BRANCH" ] && REPO_BRANCH='3.0'
 debug_mode='0'
 fork_maintainer='0'
